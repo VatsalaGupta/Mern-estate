21239d539499a4dd02aaa0b173260682b8ca23ef
Vatsala Gupta <vatsala00000gupta@gmail.com>
2024-02-08 23:44:16 +0530

    complete signup functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  Server:{
+    proxy: {
+      '/api': 
+      {
+        target : 'https://localhost:3000',
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
